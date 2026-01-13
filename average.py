aad6190960c1f45d2d72c54e01c7de52371177bd
Shakti-195 <thakurshaktisingh195@gmail.com>
2026-01-13 18:00:10 +0530

    Arithmetic operator related programs

---
+++ average.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the fifth number: "))
+
+average = (num1 + num2 + num3 + num4 + num5) / 5
+
+print("Average =", average)
---
+++ totalcost.py
@@ -0,0 +1,9 @@
+pen_price = 15
+notebook_price = 20
+
+pen = 3
+notebook = 5
+
+total_cost = (pen_price*pen)+(notebook_price*notebook)
+
+print("total cost is :",total_cost)
