6516aa6ab348c70fbf080e77de3d890ad4ed57c3
Shakti-195 <thakurshaktisingh195@gmail.com>
2026-01-23 16:42:57 +0530

    modules completed

---
+++ module.py
@@ -0,0 +1,49 @@
+# math module 
+
+#1. Find the square root of 625
+
+import math 
+print (math.sqrt(625))
+
+#2 Calculate the factorial of 6
+import math
+print(math.factorial(6))
+
+#3. Find the floor and ceiling values of 4.7
+import  math 
+print(math.floor(4.7))
+print(math.ceil(4.7))
+
+#4. Compute 5 raised to the power 3 using math module
+import math 
+print(math.pow(5,3))
+
+#5. Find the absolute value of -18 using math.fabs()
+import math
+print (math.fabs(-18))
+
+#6. Find sine, cosine, tangent of 90 degrees
+import math
+
+
+angle  = math.radians(90)
+
+print(math.sin(angle))
+print(math.cos(angle))
+print(math.tan(angle))
+
+#7.Find GCD of 36 and 60
+import math
+print(math.gcd(36,60))
+
+#8. Find the value of e³
+import math
+print(math.exp(3))
+
+#9.Find log base 10 of 1000
+import math
+print(math.log10(1000))
+
+#10.Convert 180 degrees into radians
+import math
+print(math.radians(180))
